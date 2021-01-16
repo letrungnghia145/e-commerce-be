d4167191412fdf29b6539dc37098c995d0ce2276
daotiensyyyy <sydao1579@gmail.com>
2021-01-16 13:27:49 +0700

    permission & role

--- src/main/java/com/tmdt/app/enums/Permission.java
+++ src/main/java/com/tmdt/app/enums/Permission.java
@@ -3,8 +3,30 @@ package com.tmdt.app.enums;
 public enum Permission {
 	//Chia quyen theo role
 	//dinh nghia tat ca quyen co trong he thong
-	POST("create:post");
 	
+	//bai dang
+	POST_CREATE("post:create"),
+	POST_UPDATE("post:update"),
+	POST_DELETE("post:delete"),
+	POST_VIEW_MANAGEMENT("post:view"),
+	
+	//profile
+	PROFILE_CREATE("profile:create"),
+	PROFILE_UPDATE("profile:update"),
+	
+	//tai khoan
+	ACCOUNT_ALL("account:get"),
+	ACCOUNT_UPGRADE("account:upgrade"),
+	ACCOUNT_MANAGEMENT("account:management"),
+	
+	//goi dich vu
+	SERVICE_PACK_CURRENT("pack:current"),	//xem goi dich vu hien tai
+	SERVICE_PACK_CREATE("pack:create"),
+	SERVICE_PACK_UPDATE("pack:update"),
+	SERVICE_PACK_DELETE("pack:delete"),
+	
+	//loi nhuan
+	PROFIT("profit:management");
 
 	private String permission;
 
--- src/main/java/com/tmdt/app/enums/UserRole.java
+++ src/main/java/com/tmdt/app/enums/UserRole.java
@@ -5,7 +5,30 @@ import java.util.Set;
 import com.google.common.collect.Sets;
 
 public enum UserRole {
-	ROLE_ADMIN(Sets.newHashSet(Permission.POST)), ROLE_EMPLOYEE(Sets.newHashSet()), ROLE_EMPLOYER(Sets.newHashSet());
+	ROLE_ADMIN(Sets.newHashSet(
+			Permission.POST_CREATE,
+			Permission.POST_DELETE,
+			Permission.POST_UPDATE,
+			Permission.POST_VIEW_MANAGEMENT,
+			Permission.ACCOUNT_ALL,
+			Permission.ACCOUNT_UPGRADE,
+			Permission.ACCOUNT_MANAGEMENT,
+			Permission.SERVICE_PACK_CREATE,
+			Permission.SERVICE_PACK_UPDATE,
+			Permission.SERVICE_PACK_DELETE,
+			Permission.PROFIT
+	)), 
+	
+	ROLE_EMPLOYEE(Sets.newHashSet(
+			Permission.PROFILE_CREATE,
+			Permission.PROFILE_UPDATE
+	)), 
+	
+	ROLE_EMPLOYER(Sets.newHashSet(
+			Permission.PROFILE_CREATE,
+			Permission.PROFILE_UPDATE,
+			Permission.SERVICE_PACK_CURRENT
+	));
 
 	public Set<Permission> permissions;
 
